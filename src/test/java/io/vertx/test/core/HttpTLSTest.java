89f33a1158c3349ea6fa1331efd51199e0f3b4b6
alexlehm <alexlehm@gmail.com>
2016-04-20 00:27:11 +0200

    add proxy test implementation and a first version of the unit test
    Signed-off-by: alexlehm <alexlehm@gmail.com>

--- src/test/java/io/vertx/test/core/HttpTLSTest.java
+++ src/test/java/io/vertx/test/core/HttpTLSTest.java
@@ -285,6 +285,7 @@ public abstract class HttpTLSTest extends HttpTestBase {
     boolean serverUsesCrl;
     boolean serverOpenSSL;
     boolean serverUsesAlpn;
+    boolean useProxy;
     String[] clientEnabledCipherSuites = new String[0];
     String[] serverEnabledCipherSuites = new String[0];
     String[] clientEnabledSecureTransportProtocol   = new String[0];
@@ -358,6 +359,11 @@ public abstract class HttpTLSTest extends HttpTestBase {
       return this;
     }
 
+    TLSTest useProxy() {
+      useProxy = true;
+      return this;
+    }
+
     void pass() {
       run(true);
     }
@@ -389,6 +395,10 @@ public abstract class HttpTLSTest extends HttpTestBase {
       }
       for (String protocols: clientEnabledSecureTransportProtocol) {
         options.addEnabledSecureTransportProtocol(protocols);
+      }
+      if (useProxy) {
+        options.setProxyHost("localhost");
+        options.setProxyPort(13128);
       }
       client = createHttpClient(options);
       HttpServerOptions serverOptions = new HttpServerOptions();
